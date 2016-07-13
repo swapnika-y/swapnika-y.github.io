1a483f1fc3b91c7acaef864f58fbcc205e7f65c8
swapnika.yerramsetty <swapnika.yerramsetty@lge.com>
2016-07-13 17:04:14 +0530

    nav change

--- layoutNav.js
+++ layoutNav.js
@@ -1,4 +1,26 @@
-
-function mainNavItemClick(){
-
+function mainNavItem2ClickHandler() {
+	console.log("in Item2ClickHandler");
+	document.getElementById('subUL3').style.display = 'none';
+	document.getElementById('subUL1').style.display = 'none';
+	document.getElementById('subUL2').style.display = 'block';
+	document.getElementById("subUL2").className = "nav1left";
 }
+function mainNavItem1ClickHandler() {
+	console.log("in Item2ClickHandler");
+	document.getElementById('subUL3').style.display = 'none';
+	document.getElementById('subUL2').style.display = 'none';
+	document.getElementById('subUL1').style.display = 'block';
+	document.getElementById("subUL1").className = "nav1left";
+}
+function addEventListenerByClass(className, event, fn) {
+	var i;
+    var list = document.getElementsByClassName(className);
+    for (i = 0, len = list.length; i < len; i++) {
+        list[i].addEventListener(event, fn, false);
+    }
+}
+(function () {
+	console.log("im IIFE");
+	document.querySelector("#mainItem1").addEventListener("click",mainNavItem1ClickHandler,false);
+	document.querySelector("#mainItem2").addEventListener("click",mainNavItem2ClickHandler,false);
+})();
