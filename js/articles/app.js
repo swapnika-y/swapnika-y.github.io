388b83494c57dae93e823687a9db7769988c74e6
I333245 <rajkumar.selvam@sap.com>
2016-07-28 13:21:50 +0530

    Fix JSON error, fix html to fetch category, date and descriptopn properly

--- js/articles/app.js
+++ js/articles/app.js
@@ -4,6 +4,7 @@ myApp.controller("ArticleController", function ($scope, $http) {
     $http.get('./resources/data/articles.data.json').
     success(function (data) {
         $scope.articles = data.articles;
+        console.log($scope.articles);
     }).
     error(function (error) {
         console.log("Error reading articles.json: " + error);
