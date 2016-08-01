4263accacb87f60bf04818f92379cad11495ea68
swapnika.yerramsetty <swapnika.yerramsetty@lge.com>
2016-08-01 14:33:50 +0530

    Category selection

--- js/articles/app.js
+++ js/articles/app.js
@@ -1,54 +1,84 @@
 var myApp = angular.module("myApp", []);
-/*angular.module('myApp', [])
-.run(function($rootScope) {
-    $rootScope.test = new Date();
-})*/
-myApp.controller("CategoryController", function ($scope, $http) {
-    $http.get('./resources/data/articles.data.json').
-    success(function (data) {
-        $scope.categories = data.articles;
-		$scope.categories = fetchCategories(data.articles);
-        console.log($scope.categories);
-    }).
-    error(function (error) {
-        console.log("Error reading articles.json: " + error);
-    });
+
+myApp.service('articleService', function () {
+	this.articles = [];
+	this.getArticles = function () {
+		return this.articles;
+	};
+	this.setArticles = function (articlesList) {
+		this.articles = articlesList;
+	};
+	this.addNewArticles = function (newArticlesList) {
+		this.articles = this.articles.concat(newArticlesList);
+	};
+
+});
+/*function ctrl($scope) {
+  $scope.itemList = [];
+  $scope.blisterPackTemplates = [{id:1,name:"a"},{id:2,name:"b"},{id:3,name:"c"}];
+
+  $scope.changedValue = function(item) {
+    $scope.itemList.push(item.name);
+  }       
+}*/
+
+myApp.controller("CategoryController", function ($scope, $http, articleService) {
+	console.log("CategoryController: ");
+	//Selected default value of dropdown
+	$http.get('./resources/data/articles.data.json').
+	success(function (data) {
+		//$scope.categories = data.articles;
+		articleService.setArticles(data.articles);
+		$scope.categories = fetchCategories(articleService.getArticles());
+		console.log($scope.categories);
+	}).
+	error(function (error) {
+		console.log("Error reading articles.json: " + error);
+	});
+	$scope.onCategoryChange = function(item){
+		console.dir("item");
+		console.dir(item);
+		console.dir($scope.item);
+	};
 });
 
-myApp.controller("ArticleController", function ($scope, $http) {
-    $http.get('./resources/data/articles.data.json').
-    success(function (data) {
-        $scope.articles = data.articles;
-        //console.log($scope.articles);
+myApp.controller("ArticleController", function ($scope, $http, articleService) {
+	console.log("ArticleController: "+articleService.getArticles());
+	$scope.articles = categorizeArticles(articleService.getArticles(), "JS");
+	/*$http.get('./resources/data/articles.data.json').
+	success(function (data) {
+		$scope.articles = data.articles;
+		//console.log($scope.articles);
 		$scope.articles = categorizeArticles($scope.articles, "JS");
-    }).
-    error(function (error) {
-        console.log("Error reading articles.json: " + error);
-    });
+	}).
+	error(function (error) {
+		console.log("Error reading articles.json: " + error);
+	});*/
 });
 
-function categorizeArticles(array, category){
+function categorizeArticles(array, category) {
 	var filteredList = [];
-	if(array.length >0){
-		filteredList = array.filter(function(item, idx, array){
-			if(item.category === category){
+	if (array.length > 0) {
+		filteredList = array.filter(function (item, idx, array) {
+			if (item.category === category) {
 				return true;
 			}
 			return false;
 		}, this);
-	}else{
+	} else {
 		filteredList = array;
 	}
+	console.dir(filteredList);
 	return filteredList;
 }
 
-function fetchCategories(array){
+function fetchCategories(array) {
 	var resultCategoryList = [];
-	var i=0, l = array.length;
-	console.dir(array);
-	console.dir(l);
-	for(i=0;i<l;i++){
-		if(resultCategoryList.indexOf(array[i].category) == -1){
+	var i = 0,
+		l = array.length;
+	console.dir("fetchCategories"+array);
+	for (i = 0; i < l; i++) {
+		if (resultCategoryList.indexOf(array[i].category) == -1) {
 			resultCategoryList.push(array[i].category);
 		}
 	}
