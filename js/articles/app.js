fa87acc5372fdd3f6eae4f676b9816b280b9a558
I333245 <rajkumar.selvam@sap.com>
2016-08-01 15:58:59 +0530

    Load JSON in service. Swap need to check the select tag binding issue

--- js/articles/app.js
+++ js/articles/app.js
@@ -1,41 +1,78 @@
 var myApp = angular.module("myApp", []);
 
 myApp.service('articleService', function ($http) {
-	this.articles = [];
-	$http.get('./resources/data/articles.data.json').
-	success(function (data) {
-		this.articles = = data.articles;
-	}).
-	error(function (error) {
-		console.log("Error reading articles.json: " + error);
-	});
-	this.getArticles = function () {
-		return this.articles;
-	};
-	/*this.setArticles = function (articlesList) {
-		this.articles = articlesList;
-	};*/
-	this.addNewArticles = function (newArticlesList) {
-		this.articles = this.articles.concat(newArticlesList);
-	};
+    this.getArticlesAsync = function () {
+        // $http returns a promise
+        var promise = $http.get('./resources/data/articles.data.json');
+        return promise;
+    };
+
+    this.addNewArticles = function (newArticlesList) {
+        this.articles = this.articles.concat(newArticlesList);
+    };
+
+    /*this.articles = [];
+    $http.get('./resources/data/articles.data.json').
+    success(function (data) {
+        this.articles = data.articles;
+        console.log("articleService: ", data.articles);
+    }).
+    error(function (error) {
+        console.log("Error reading articles.json: " + error);
+    });
+
+    this.getArticles = function () {
+        return this.articles;
+    };
+
+    //this.setArticles = function (articlesList) {
+	//	this.articles = articlesList;
+	//};
+
+    this.addNewArticles = function (newArticlesList) {
+        this.articles = this.articles.concat(newArticlesList);
+    };*/
 
 });
 
 myApp.controller("CategoryController", function ($scope, articleService) {
-	console.log("CategoryController: ");
-	//Selected default value of dropdown
-	$scope.categories = fetchCategories(articleService.getArticles());
-	console.log($scope.categories);
-	$scope.onCategoryChange = function (item) {
-		console.dir("item");
-		console.dir(item);
-		console.dir($scope.item);
-	};
+    console.log("CategoryController");
+    articleService.getArticlesAsync()
+        .then(function successCallback(response) {
+            // this callback will be called asynchronously
+            // when the response is available
+            $scope.categories = fetchCategories(response.data.articles);
+            console.log($scope.categories);
+            $scope.onCategoryChange = function (item) {
+                console.dir("item");
+                console.dir(item);
+                console.dir($scope.item);
+            };
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            console.log("errorCallback");
+        });
+
+    /*console.log("CategoryController: ");
+    $scope.categories = fetchCategories(articleService.getArticles());
+    console.log($scope.categories);
+    $scope.onCategoryChange = function (item) {
+        console.dir("item");
+        console.dir(item);
+        console.dir($scope.item);
+    };*/
 });
 
 myApp.controller("ArticleController", function ($scope, $http, articleService) {
-	console.log("ArticleController: " + articleService.getArticles());
-	$scope.articles = categorizeArticles(articleService.getArticles(), "JS");
+    console.log("ArticleController");
+    articleService.getArticlesAsync()
+        .then(function (response) {
+            $scope.articles = categorizeArticles(response.data.articles, "CSS");
+            console.log($scope.articles);
+        });
+
+    /*$scope.articles = categorizeArticles(articleService.getArticles(), "JS");*/
 });
 
 function categorizeArticles(array, category) {
